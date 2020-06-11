5660121147c577766acce1b0832634737f9474a2
RA404 <Repin404@gmail.com>
2020-06-11 18:29:44 +0500

    fixed articles model

--- models/articles.js
+++ models/articles.js
@@ -4,31 +4,22 @@ const validator = require('validator');
 const articlesSchema = new mongoose.Schema({
   title: {
     type: String,
-    minlength: 2,
-    maxlength: 100,
     required: true,
   },
   text: {
     type: String,
-    minlength: 2,
     required: true,
   },
   keyword: {
     type: String,
-    minlength: 2,
-    maxlength: 50,
     required: true,
   },
   date: {
     type: String,
-    minlength: 6,
-    maxlength: 10,
     required: true,
   },
   source: {
     type: String,
-    minlength: 2,
-    maxlength: 50,
     required: true,
   },
   link: {
