6bb4b9d6298d67a8f04568289c5df1c089acac19
RA404 <Repin404@gmail.com>
2020-06-11 15:42:53 +0500

    fixed articles validation

--- schemas/articles.js
+++ schemas/articles.js
@@ -1,38 +1,33 @@
 const joi = require('joi');
 const ErrorBadRequest = require('../errors/ErrorBadRequest');
 
+const urlPattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+
 module.exports = joi.object().keys({
   title: joi.string()
     .required()
-    .min(2)
-    .max(100)
-    .error(() => new ErrorBadRequest({ message: 'The title field must be longer than 2 and less than 100 characters' })),
+    .error(() => new ErrorBadRequest({ message: 'The title field must be of string type' })),
   keyword: joi.string()
     .required()
-    .min(2)
-    .max(50)
-    .error(() => new ErrorBadRequest({ message: 'The keyword field must be longer than 2 and less than 50 characters' })),
+    .error(() => new ErrorBadRequest({ message: 'The keyword field must be of string type' })),
   text: joi.string()
     .required()
-    .min(2)
-    .error(() => new ErrorBadRequest({ message: 'The text field must be longer than 2' })),
+    .error(() => new ErrorBadRequest({ message: 'The text field must be of string type' })),
   source: joi.string()
     .required()
-    .min(2)
-    .max(50)
-    .error(() => new ErrorBadRequest({ message: 'The source field must be longer than 2 and less than 50 characters' })),
+    .error(() => new ErrorBadRequest({ message: 'The source field must be of string type' })),
   link: joi.string()
     .required()
     .uri()
+    .regex(urlPattern)
     .error(() => new ErrorBadRequest({ message: 'Invalid link url' })),
   image: joi.string()
     .required()
     .uri()
+    .regex(urlPattern)
     .error(() => new ErrorBadRequest({ message: 'Invalid image url' })),
   owner: joi.string(),
   date: joi.string()
     .required()
-    .min(6)
-    .max(10)
-    .error(() => new ErrorBadRequest({ message: 'The date field must be longer than 6 and less than 10 characters' })),
+    .error(() => new ErrorBadRequest({ message: 'The date field must be of string type' })),
 });
